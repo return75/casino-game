176e6c2f79ade029a5a207dd3085c92b300b64c0
atashfaraz <atashfaraz8@gmail.com>
2023-02-13 20:13:14 +0330

    refactor

---
+++ src/components/CashOutBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button id="cash-out" @mouseover="translateRandomly($event)">cash out</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CashOutBtn.vue",
+  methods: {
+    translateRandomly(e) {
+      let chance = Math.random()
+      if(chance <= .5) {
+        let direction = ['+', '-'][Math.floor(Math.random() * 2)]
+        e.target.style.transform = `translateX(${direction}300px)`
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#cash-out {
+  transition: transform .2s ease-in-out;
+}
+</style>
--- src/components/CasinoTable.vue
+++ src/components/CasinoTable.vue
@@ -3,21 +3,25 @@
     <div class="score">SCORE: {{score}}</div>
     <table>
       <tr class="d-flex">
-        <table-block ref="firstBlock" @set-new-letter="letters.first = $event"></table-block>
-        <table-block ref="secondBlock" @set-new-letter="letters.second = $event"></table-block>
-        <table-block ref="thirdBlock" @set-new-letter="letters.third = $event"></table-block>
+        <table-block ref="firstBlock" @set-new-letter="letters.first = $event" />
+        <table-block ref="secondBlock" @set-new-letter="letters.second = $event" />
+        <table-block ref="thirdBlock" @set-new-letter="letters.third = $event" />
       </tr>
     </table>
-    <button id="roll-btn" @click="createRoll" :disabled="isRollButtonDisabled">new roll</button>
+    <div>
+      <button id="roll-btn" @click="createRoll" :disabled="isRollButtonDisabled">new roll</button>
+    </div>
+    <cash-out-btn />
   </div>
 </template>
 
 <script>
 import TableBlock from "./TableBlock.vue";
+import CashOutBtn from "./CashOutBtn.vue";
 
 export default {
   name: "CasinoTable.vue",
-  components: {TableBlock},
+  components: {TableBlock, CashOutBtn},
   data() {
     return {
       score: 10,
@@ -37,14 +41,28 @@ export default {
   methods: {
     createRoll() {
       this.isResultLoading = true
-      this.$refs.firstBlock.reRoll()
-      this.$refs.secondBlock.reRoll()
-      this.$refs.thirdBlock.reRoll()
-      this.$refs.firstBlock.showLetterWithDelay(1000)
-      this.$refs.secondBlock.showLetterWithDelay(2000)
-      this.$refs.thirdBlock.showLetterWithDelay(3000).then(() => {
+      this.reRoll().then(() => {
         this.checkTheResult()
+      })
+      this.showLettersWithDelay().then(() => {
         this.isResultLoading = false
+      })
+    },
+    reRoll() {
+      return new Promise(resolve => {
+        this.$refs.firstBlock.reRoll()
+        this.$refs.secondBlock.reRoll()
+        this.$refs.thirdBlock.reRoll()
+        resolve()
+      })
+    },
+    showLettersWithDelay() {
+      return new Promise(resolve => {
+        this.$refs.firstBlock.showLetterWithDelay(1000)
+        this.$refs.secondBlock.showLetterWithDelay(2000)
+        this.$refs.thirdBlock.showLetterWithDelay(3000).then(() => {
+          resolve()
+        })
       })
     },
     checkTheResult() {
@@ -52,35 +70,50 @@ export default {
           this.letters.second?.name === this.letters.third?.name
       ) {
         // user has won the game
-        this.giveUserScore(this.letters)
+        if(this.score < 40) {
+          this.giveUserScore()
+        } else if (this.score >= 40 && this.score < 60) {
+          let chance = Math.random()
+          if(chance <= .3) {
+            this.reRoll().then(() => {
+              this.checkTheResult()
+            })
+          } else {
+            this.giveUserScore()
+          }
+        } else {
+          let chance = Math.random()
+          if(chance <= .6) {
+            this.reRoll().then(() => {
+              this.checkTheResult()
+            })
+          } else {
+            this.giveUserScore()
+          }
+        }
       } else {
         // user has failed
         this.reduceScore()
       }
     },
-    giveUserScore(letters) {
-      let score = letters.first.score
+    giveUserScore() {
+      let score = this.letters.first.score
       this.score += score
     },
     reduceScore() {
-      this.score--
+      this.showLettersWithDelay().then(() => {
+        this.score--
+      })
     },
   }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .d-flex {
   display: flex;
 }
-#roll-btn {
-  background: transparent;
-  border: 1px solid white;
-  margin-top: 3rem;
-  font-size: 2rem;
-  text-transform: uppercase;
-  padding: 0.3em .6em;
-  border-radius: 1rem;
+#roll-btn, #cash-out {
   &:hover {
     background: rgba(255, 255, 255, 0.38);
   }
