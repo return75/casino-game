6bcfaab74d96d531ab80fc088c50bad077b4fa3d
atashfaraz <atashfaraz8@gmail.com>
2023-02-13 21:01:33 +0330

    update

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,10 @@
   <casino-table></casino-table>
 </template>
 
-<script lang="ts">
+<script>
 import CasinoTable from './components/CasinoTable.vue'
 export default {
   name: "TableBlock.vue",
   components: {CasinoTable},
-
 }
 </script>
-<style scoped>
-
-</style>
